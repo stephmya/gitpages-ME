f37ac978b8fb66b02222643717c5f6d7b1171e4f
Stephanie Myalik <myalikss1@sunypoly.edu>
2024-09-23 15:34:53 -0400

    mini update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import DropdownMenu from './components/mainpage';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DropdownMenu />
     </div>
   );
 }
